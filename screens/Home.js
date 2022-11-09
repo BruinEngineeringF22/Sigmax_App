274bfe9e2947436012a083987aaeafae49a11764
Jack Lin <jacklin1229@gmail.com>
2022-11-09 11:27:06 -0800

    Added Screens and Components Folder, and added basic screens layout

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Page, this is where buttons go</Text>
+    </View>
+  );
+};
+
+export default Home;

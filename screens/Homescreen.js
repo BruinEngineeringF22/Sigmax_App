55846dd8e1abcce986c15eff9df85aab32a9a95a
osphia <sophiaylee23@gmail.com>
2022-11-12 15:35:17 -0800

    Create a file

---
+++ screens/Homescreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Homescreen = () => {
+  return (
+    <View>
+      <Text>Homescreen Page</Text>
+    </View>
+  );
+};
+
+export default Homescreen;

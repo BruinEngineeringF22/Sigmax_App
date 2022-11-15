7ece3f129e21a5cda14267d6bd24a9c1208ace8b
osphia <sophiaylee23@gmail.com>
2022-11-15 00:53:13 -0800

    first homescreen

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,12 +1,41 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import React from "react";
 
+
 const Homescreen = () => {
+    
+  onPressBandage = () => {
+    console.log("call python api")
+  }
+
   return (
-    <View>
-      <Text>Homescreen Page</Text>
+    <View style = {styles.container}>
+      <Text>Homescreen</Text>
+      <Button 
+        title = "Bandages"
+        color = "#3A4454"
+        onPress = {onPressBandage}
+      />
+      <Button 
+        title = "Pills"
+        color = "#3A4454"
+        onPress = {onPressBandage}
+      />
+      <Button 
+        title = "Miscellaneous"
+        color = "#3A4454"
+        onPress = {onPressBandage}
+      />
     </View>
   );
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF8F0',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
 export default Homescreen;

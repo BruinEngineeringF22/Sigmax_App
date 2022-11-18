e7b5a366c1fb1c4b4c8e79a2c709bf2c06e29bd2
Jack Lin <jacklin1229@gmail.com>
2022-11-18 09:07:46 -0800

    First commit to bluetooth frontend

--- PythonService.js
+++ PythonService.js
@@ -1,25 +1,25 @@
-
-
 class PythonService {
-    sendSignal(input) {
-        console.log("sendSignal called")
-        return fetch('http://127.0.0.1:5000/send-signal', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "param": input
-            })
-        }).then (response => response.text())
-          .then (response => {
-            console.log(response)
-        }).catch((error) => {
-            console.error(error);
-          });
-    }
-    
+  // old url: 'http://127.0.0.1:5000/send-signal'
+  sendSignal(input) {
+    console.log("sendSignal called");
+    return fetch("http://192.168.56.1:5000/send-signal", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        param: input,
+      }),
+    })
+      .then((response) => response.text())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
 
 export default new PythonService();
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 2. To run the app, run $npm start in the terminal
 3. If you are running the app from windows, use your phone to scan the QR code
 4. If you are running app from mac, press i in the terminal to open a IOS simulator
+
+To run Flask backend, run
+$flask --app BluetoothController run
